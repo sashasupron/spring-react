4cd8b8d96b139209cadbc74cb585601636731ca9
sasha.supron <sasha.supron3@icloud.com>
2025-07-29 12:36:16 +0300

    feat: server

--- client/src/redux/reducers/authReducers.js
+++ client/src/redux/reducers/authReducers.js
@@ -6,8 +6,6 @@ function authReducer(state = initialState, action) {
   switch (action.type) {
     case "LOGIN":
       return { ...state, isAuthenticated: true };
-    case "LOGOUT":
-      return { ...state, isAuthenticated: false };
     default:
       return state;
   }
